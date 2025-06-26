328c1b93fb89297d3564a4d6c7f9618cf1fd47de
mohamed67471 <mohamedhassan22@hotmail.co.uk>
2025-06-26 14:12:38 +0100

    Remove extra files

--- Untitled-1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-age=25 
-
-if [ $age -gt 18 ]
-then
-    echo "You are a adult"
-fi
--- tempCodeRunnerFile.sh
+++
@@ -1,7 +0,0 @@
-
-get_file_name() {
-  local file_path="$1"
-  local file_count=$(ls "$file_path" | wc -l)
-  echo "number of files in $file_path: $file_count"
-}
-get_file_name "./"
